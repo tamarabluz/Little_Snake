3205b25519bd7e034aacee8ee3d4d163f598b276
Tamara Luz <tamarabluz@gmail.com>
2022-07-13 14:49:49 -0300

    subindo classe Snake

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,7 @@
+
+//A classe Cell representa o único ponto de exibição/placa.
+// Ele contém a linha não, coluna não e as informações sobre ele,
+// ou seja, está vazio ou há comida nele ou é parte do corpo da cobra?
 public class Cell {
     private final int row, col;
     private Celltype celltype;
--- src/Snake.java
+++ src/Snake.java
@@ -1,3 +1,41 @@
-public class Snake {
+//Aclasse Snake, que contém o corpo e a cabeça.
+//Usamos Linked List para armazenar o corpo porque podemos adicionar uma célula em O(1).
+//O método Grow será chamado quando ele comer o alimento. Outros métodos são autoexplicativos.
 
+import java.util.LinkedList;
+
+public class Snake {
+    provate LinkedList<Cell>
+    snakePartList = new java.util.LinkedList<>();
+    private Cell head;
+    public Snake(Cell initPos){
+        head = initPos;
+        snakePartList.add(head);
+        head.setCelltype(CellType.SNAKE_NODE);
+    }
+
+    public void grow(){ snakePartList.add(head);}
+
+    public void move(Cell nextCell){
+        System.out.println("Going to check for Crash");
+        for (Cell cell : snakePartList){
+            if (cell == nextCell){
+                return true;
+            }
+        }
+        return false;
+    }
+    public LinkedList<Cell> getSnakePartList(){
+        return SnakePartList;
+    }
+
+    public void setSnakePartList(LinkedList<Cell> snakePartList){
+        this.getSnakePartList = snakePartList;
+    }
+    public Cell getHead(){
+        return head;}
+
+    public void setHead(Cell head) {
+        this.head = head;
+    }
 }
